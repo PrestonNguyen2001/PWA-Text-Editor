f42351c8921b883a6fe43518b9e52994ada780f5
PrestonNguyen2001 <prestonnguyen2001@gmail.com>
2024-06-30 20:00:20 -0400

    fixed server

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ app.use(express.static(path.join(__dirname, "client/dist")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require("./routes/htmlRoutes")(app);
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
